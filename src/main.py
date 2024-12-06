68b4d83c40ad7694e3dc8e9c721a7d79bccb60eb
vedaRadev <ryancarmstrong97@gmail.com>
2024-12-06 12:57:27 -0500

    simple text node stuff

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from textnode import TextNode, TextType
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
+    print(node)
