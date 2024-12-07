bc1df6054cf34ec3b0da5d12d79ac9c50a184a1e
vedaRadev <ryancarmstrong97@gmail.com>
2024-12-07 14:36:19 -0500

    add leafnode (parent: HTMLNode)

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -4,16 +4,18 @@ class HTMLNode:
     # No children => assumed to have value
     # No props => no attributes
     def __init__(self, tag = None, value = None, children = None, props = None):
-        assert value or children
+        # assert value or children
 
         self.tag = tag
         self.value = value
         self.children = children
         self.props = props
 
+
     def to_html(self):
         raise NotImplementedError
     
+
     # TODO default props to empty dictionary
     def props_to_html(self):
         result = ""
@@ -26,5 +28,23 @@ class HTMLNode:
 
         return result
 
+
     def __repr__(self):
         return f"tag: {self.tag}\nvalue: {self.value}\nchildren: {self.children}\nprops: {self.props}"
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props = None):
+        super().__init__(tag, value, None, props)
+
+
+    def to_html(self):
+        # TODO move this up to the constructor
+        if not self.value:
+            raise ValueError("leaf nodes must have values")
+
+        if not self.tag:
+            return f"{self.value}"
+        
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,9 +1,20 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_props_to_html(self):
         node = HTMLNode(value = "whatever", props = { "href": "google.com", "target": "_blank" })
         html_attribs = node.props_to_html()
         self.assertEqual(' href="google.com" target="_blank"', html_attribs)
+
+
+class TestHTMLLeafNode(unittest.TestCase):
+    def test_with_value(self):
+        leaf_node = LeafNode("a", "google.com", { "prop1": "prop1", "prop2": "prop2" })
+        leaf_node_html = leaf_node.to_html()
+        self.assertEqual(leaf_node_html, '<a prop1="prop1" prop2="prop2">google.com</a>')
+
+    def test_with_no_value(self):
+        leaf_node = LeafNode("a", None, { "prop1": "prop1", "prop2": "prop2" })
+        self.assertRaises(ValueError, leaf_node.to_html)
